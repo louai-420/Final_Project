38631de0cb78bb94687a6389b6bf1edc7c25abd1
louai-420 <119933424+louai-420@users.noreply.github.com>
2025-04-10 00:42:07 +0100

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,4 +1,4 @@
-// backend/routes/userRoutes.js
+
 const express = require("express");
 const userController = require("../Controllers/userController");
 const { authMiddleware, isAdmin } = require("../middleware/authMiddleware");
